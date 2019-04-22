000e3a8ca69b1d117fc1e5c4f91e95eab48447ec
laosrob <robertchen0@yahoo.com>
2019-04-22 15:03:38 -0500

    first_commit

--- server.js
+++ server.js
@@ -1,15 +1,33 @@
 const express = require('express');
+var bodyParser = require('body-parser');
+var path = require("path");
 const app = express();
 app.use(express.static(__dirname + "/public"));
-const server = app.listen(1337);
-const io = require('socket.io')(server);
 var counter = 0;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "./static")));
+app.set('views', path.join(__dirname, './views'));
+app.set('view engine', 'ejs');
+
+app.get('/', function (req, res) {
+  res.render("index");
+});
+
+var server = app.listen(8000, function () {
+  console.log("listening on port 8000");
+});
+
+var io = require('socket.io').listen(server);
+var clients = io.sockets.clients();
     
-io.on('connection', function (socket) { //2
-  
-  socket.emit('greeting', { msg: 'Greetings, from server Node, brought to you by Sockets! -Server' }); //3
-  socket.on('thankyou', function (data) { //7
-    console.log(data.msg); //8 (note: this log will be on your server's terminal)
-  });
+  io.sockets.on('connection', function (socket) {
+    console.log("Client/socket id is: ", socket.id);
+    socket.on('connect', function() { connectCounter++; });
+    socket.emit('connection_response',{ response : socket.id});
+
+    socket.on("new_user", function (user) {
+        io.emit('display_new_user', { response: user.name });
+    });
     
 });
