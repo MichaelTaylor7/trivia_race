6d5c717f7fdcc1b83d726edf720f58d0fa6e9dc1
laosrob <robertchen0@yahoo.com>
2019-04-22 17:41:31 -0500

    adding users

--- server.js
+++ server.js
@@ -2,8 +2,9 @@ const express = require('express');
 var bodyParser = require('body-parser');
 var path = require("path");
 const app = express();
+app.engine("html", require("ejs").renderFile);
 app.use(express.static(__dirname + "/public"));
-var counter = 0;
+var users = {};
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "./static")));
@@ -11,7 +12,7 @@ app.set('views', path.join(__dirname, './views'));
 app.set('view engine', 'ejs');
 
 app.get('/', function (req, res) {
-  res.render("index");
+  res.render("index", { users: users });
 });
 
 var server = app.listen(8000, function () {
@@ -19,15 +20,24 @@ var server = app.listen(8000, function () {
 });
 
 var io = require('socket.io').listen(server);
-var clients = io.sockets.clients();
-    
-  io.sockets.on('connection', function (socket) {
-    console.log("Client/socket id is: ", socket.id);
-    socket.on('connect', function() { connectCounter++; });
-    socket.emit('connection_response',{ response : socket.id});
 
-    socket.on("new_user", function (user) {
-        io.emit('display_new_user', { response: user.name });
-    });
-    
+io.sockets.on('connection', function (socket) {
+  console.log("Client/socket id is: ", socket.id);
+  socket.emit('connection_response', { response: socket.id });
+
+  socket.on("new_user", function (user) {
+    users[socket.id] = user.name
+    console.log("user id" + socket.id)
+    console.log(users)
+    socket.emit('connect_user', { response: user.name });
+    io.emit('display_update_users', { response: user.name });
+  });
+
+  socket.on("disconnect", function () {
+    name = users[socket.id]
+    console.log("User disconnected " + name)
+    delete users[socket.id];
+    console.log(users)
+    io.emit('disconnect_user', { response: name });
+  });
 });
