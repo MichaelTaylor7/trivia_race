b59079b4003f1dcc936cd55d69bc736827ceac78
laosrob <robertchen0@yahoo.com>
2019-04-26 13:30:16 -0500

    final change for presenation

--- server.js
+++ server.js
@@ -12,6 +12,7 @@ var count = 0;
 var question_count = 0;
 var answers = [];
 var count_answered = 0;
+var right_list = [];
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "./static")));
@@ -34,6 +35,9 @@ io.sockets.on('connection', function (socket) {
   socket.emit('connection_response', { response: socket.id });
 
   socket.on("new_user", function (newuser) {
+    if (users.length > 5) {
+      return;
+    }
     user = {
       _id: trueId,
       name: newuser.name,
@@ -64,7 +68,6 @@ io.sockets.on('connection', function (socket) {
   });
 
   socket.on("action", function (id) {
-    console.log('--------------------')
     console.log(id)
     for (let i = 0; i < users.length; i++) {
       if (users[i]._id == id) {
@@ -87,6 +90,7 @@ io.sockets.on('connection', function (socket) {
 
   socket.on("ques", function () {
     answers = [];
+    right_list = [];
     count_answered = 0;
     ques = questions.results[question_count]
     question_count++
@@ -100,23 +104,27 @@ io.sockets.on('connection', function (socket) {
     console.log(ques)
     console.log(answers)
 
-    io.emit("question_options", ques, answers)
+    io.emit("question_options", ques, answers, users)
   });
 
   socket.on("answer", function (answer) {
     console.log(answer)
     console.log("///////////////////////////////////////////")
+    var correct = questions.results[question_count - 1].correct_answer
     console.log(questions.results[question_count - 1].correct_answer)
     console.log("Question number: ", question_count)
     for (let i = 0; i < users.length; i++) {
       if (socket.id == users[i]._id) {
         users[i].answered = true;
         if (users[i].answered == true) {
+          if (answer == questions.results[question_count - 1].correct_answer) {
+            users[i].score += 20;
+            right_list.push(users[i].name);
+          }
           count_answered++
           console.log("Count answered: ", count_answered)
           if (count_answered == users.length) {
-            if (question_count == 2) {
-              console.log("is it fucking coming here???")
+            if (question_count == 5) {
               let winner = users[0]
               for (let i = 0; i < users.length; i++) {
                 if (users[i].score > winner.score) {
@@ -125,7 +133,7 @@ io.sockets.on('connection', function (socket) {
 
               }
               console.log(winner)
-              io.emit("end_game", winner)
+              io.emit("end_game", users, winner)
               return;
             }
             for (let i = 0; i < users.length; i++) {
@@ -133,11 +141,8 @@ io.sockets.on('connection', function (socket) {
               users[i].answered = false;
             }
             console.log('update scores')
-            io.emit("update", users)
+            io.emit("update", users, correct, right_list)
           }
-        }
-        if (answer == questions.results[question_count - 1].correct_answer) {
-          users[i].score += 10;
         }
       }
 
@@ -146,4 +151,15 @@ io.sockets.on('connection', function (socket) {
     console.log(users)
   });
 
+  socket.on("reset", function () {
+    console.log("reset here")
+    users = [];
+    user = {};
+    questions;
+    count = 0;
+    question_count = 0;
+    answers = [];
+    count_answered = 0;
+    right_list = [];
+  })
 });
